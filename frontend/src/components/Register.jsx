dd5c87176b15b4aa1275e5489b276fe278f776ef
Aryan Gaire <botx711@gmail.com>
2025-03-07 12:16:36 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
-import { auth, RecaptchaVerifier, signInWithPhoneNumber, sendEmailVerification } from './firebase';
+import { auth, RecaptchaVerifier, signInWithPhoneNumber, sendEmailVerification } from '../firebase';
 
 const Register = () => {
   const [username, setUsername] = useState('');
