d859003599498915cb0589226395635b7d327a37
Aryan Gaire <botx711@gmail.com>
2025-03-08 14:32:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection (unchanged)
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
